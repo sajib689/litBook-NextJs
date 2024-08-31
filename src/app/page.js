554cbde4a4f3d04cf490c4c924b1a3eebb2d4371
sajib689 <jibonraja11@gmail.com>
2024-08-31 22:34:12 +0600

    home page design almost end

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,30 @@
 import Image from "next/image";
-import bgImg from '/public/home/bg-img.png'
+import bgImg from "/public/home/bg-img.png";
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center relative h-screen">
       <Image
-      src={bgImg}
-      alt="bgImage"
-      fill
-      sizes='100vh'
-      style={{objectFit: 'cover'}}
+        className="blur-sm"
+        src={bgImg}
+        alt="bgImage"
+        fill
+        sizes="100vh"
+        style={{ objectFit: "cover" }}
       />
-
+      <div className="flex flex-col justify-center items-center bg-purple-200 rounded-md p-2 lg:w-2/5 h-2/6 z-10">
+        <div className="flex pb-5 mb-5 text-5xl text-purple-800">
+          <p>LitBook</p>
+        </div>
+        <div>
+          <Link
+            className="bg-purple-700 text-white rounded-sm p-2"
+            href="/store"
+          >
+            Take me to the Store
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
